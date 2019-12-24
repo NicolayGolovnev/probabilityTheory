ac3d08365abfac568755669261801efa9865fb33
Nicolay Golovnev <kolya.golovnev@mail.ru>
2019-12-24 12:47:02 +0700

    Some saving files don't loaded

--- groupedRowY.cs
+++ groupedRowY.cs
@@ -23,6 +23,7 @@ namespace Zaychik
         public groupedRowY()
         {
             InitializeComponent();
+
         }
 
         private void groupedRowX_Load(object sender, EventArgs e)
@@ -125,8 +126,7 @@ namespace Zaychik
             textBox_Hx.Text = String.Format("{0:0.00}", Convert.ToString(Hx));
             textBox_extend.Text = String.Format("{0:0.00}", Convert.ToString(extendX));
 
-            //создаем форму графика
-            Graphics grph = new Graphics(2, dataIntervals, dataPoligon, dataGist, dataAverage, dataEmpiric, Hx);
+            
         }
 
         private void button_graphics_Click(object sender, EventArgs e)
@@ -135,7 +135,9 @@ namespace Zaychik
             //Для полигона - средние значения и dataPoligon
             //Для гистограммы - интервалы и dataGist
             //Для эмпирик функ - интервалы и dataEmpiric
-            
+
+            //создаем форму графика
+            Graphics grph = new Graphics(2, dataIntervals, dataPoligon, dataGist, dataAverage, dataEmpiric, Hx);
             grph.ShowDialog();
         }
     }
